93dc1d76acf5780b9be6b761d54f0198d1eee42d
EvanMing <mingboost@gmail.com>
2026-08-17 13:20:17 +0800

    deploy with docker

--- web-app/app/main/src/bootstrap.ts
+++ web-app/app/main/src/bootstrap.ts
@@ -22,7 +22,7 @@ export async function bootstrap(): Promise<void> {
  * 全局网络层初始化
  */
 function initGlobalNetwork() {
-  const baseURL = 'http://localhost:8000'
+  const baseURL = 'http://47.250.80.234:8000'
 
   initNetApi({
     baseURL,
