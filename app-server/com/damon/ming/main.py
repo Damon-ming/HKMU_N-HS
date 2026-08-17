c72f4e3fc0d3ed37a09960e69bbb008debe165a0
EvanMing <mingboost@gmail.com>
2026-08-17 10:39:45 +0800

    open domain

--- app-server/com/damon/ming/main.py
+++ app-server/com/damon/ming/main.py
@@ -16,14 +16,22 @@ from chat.router import chat_router
 from upload.router import upload_router
 
 app = FastAPI(title="我的多功能应用")
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# test
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"],  # 允许所有来源请求
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(upload_router.router)
 app.include_router(chat_router.router)
 
